32c944aa3cf146137a241b0ea1b56eb59005096d
Kien Nguyen <nguyen_kien@live.com>
2023-10-12 21:46:20 -0700

    work on the login pages index.jsx

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -27,7 +27,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem"}}>
-          Welcome this is not twitter!!!! A Platform that does not have a bird!
+          Welcome this is not twitter!! A Platform that does not have a bird!
         </Typography>
         <Form />
       </Box>
