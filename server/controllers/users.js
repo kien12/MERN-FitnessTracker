218362a07a321ce69f9c059758ec8717d9347512
Kien Nguyen <nguyen_kien@live.com>
2023-10-23 04:16:38 -0700

    fix bug where user would not save user on register

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,6 @@
-import User from '../models/User.js';
+import User from "../models/User.js";
 
-//read
+/* READ */
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
@@ -9,36 +9,35 @@ export const getUser = async (req, res) => {
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
-}
+};
 
 export const getUserFriends = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
-  
+
     const friends = await Promise.all(
       user.friends.map((id) => User.findById(id))
     );
     const formattedFriends = friends.map(
-    ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-      return { _id, firstName, lastName, occupation, location, picturePath }
-    }
-  );
-  res.status(200). json(formattedFriends);
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
+    res.status(200).json(formattedFriends);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
-}
+};
 
-//UPDATE
-
+/* UPDATE */
 export const addRemoveFriend = async (req, res) => {
   try {
     const { id, friendId } = req.params;
     const user = await User.findById(id);
     const friend = await User.findById(friendId);
 
-    if (user.friends.includes(friendId)){
+    if (user.friends.includes(friendId)) {
       user.friends = user.friends.filter((id) => id !== friendId);
       friend.friends = friend.friends.filter((id) => id !== id);
     } else {
@@ -52,13 +51,13 @@ export const addRemoveFriend = async (req, res) => {
       user.friends.map((id) => User.findById(id))
     );
     const formattedFriends = friends.map(
-    ({ _id, firstName, lastName, occupation, location, picturePath }) => {
-      return { _id, firstName, lastName, occupation, location, picturePath }
-    }
-  );
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath };
+      }
+    );
 
-  res.status(200).json(formattedFriends);
+    res.status(200).json(formattedFriends);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
-}
+};
