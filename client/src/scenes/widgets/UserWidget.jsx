b44ce2b33711230074686c50e73504997147ced9
Kien Nguyen <nguyen_kien@live.com>
2023-10-23 04:36:00 -0700

    complete UserWidget.jsx

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -1,5 +1,50 @@
-const Widgets = () => {
-  return (<div>Widgets</div>)
-}
+import {
+  ManagedAccountsOutlined,
+  EditOutlined,
+  LocationOnOutlined,
+  WorkOutlineOutlined,
+} from "@mui/icons-material";
+import { Box, Typography, Divider, useTheme} from "@mui/icons-material";
+import UserImage from "components/UserImage";
+import FlexBetween from "components/FlexBetween";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default Widgets;
+const UserWidget = ({ userId, picturePath }) => {
+  const [user, setUser] = useState(null);
+  const { palette } = useTheme();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.token);
+  const dark = palette.neutral.dark;
+  const medium = palette.neutral.medium;
+  const main = palette.neutral.main;
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []) // esling-disable-line react-hooks/exhaustive-deps 
+
+  if (!user) {
+    return null;
+  }
+
+  const {
+    firstName,
+    lastName,
+    location,
+    occupation,
+    viewedProfile,
+    impressions,
+    friends,
+  } = user;
+}
